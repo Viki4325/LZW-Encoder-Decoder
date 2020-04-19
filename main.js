d3e0b5f92071cda153ca1ca451b5c024dcdac782
Viki4325 <58233681+Viki4325@users.noreply.github.com>
2020-04-18 19:50:30 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-//-----------------------------------------
-     // NAME		: Makadia Viki Suresh 
-     // STUDENT NUMBER	: 7870989
-     // COURSE		: COMP 2150
-     // INSTRUCTOR	: Mike Domaratzki
-     // ASSIGNMENT	: assignment 4
-     // QUESTION	: question 2      
+//--------------------------------------- 
      // 
      // REMARKS: This class, takes file name through command line argument and passes this file name to
      //         the encoder object created here.
